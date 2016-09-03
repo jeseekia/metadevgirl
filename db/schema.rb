099a80045b4535bcff3f8fa97987d1829794fc88
Jeseekia Vaughn <jeseekia@gmail.com>
2016-09-03 16:25:16 -0400

    Migration done to add body column to the posts table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902192053) do
+ActiveRecord::Schema.define(version: 20160903133842) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
